2948870a90a238b2dc90790b118ab6ddc9761dc1
kaleabSolomon <kaleabslmn@gmail.com>
2024-11-27 20:26:35 +0300

    init

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (
+    <div className="w-screen min-h-screen flex items-center justify-center text-4xl">
+      Landing
+    </div>
+  );
+};
+
+export default Landing;
