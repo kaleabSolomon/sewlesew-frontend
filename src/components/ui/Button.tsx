6fd63f6a82b541c6f381d4eee8959f9c377ed00c
kaleabSolomon <kaleabslmn@gmail.com>
2024-12-03 16:29:54 +0300

    make a custom button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import clsx from "clsx";
+
+// Define button variants with CVA
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors", // Base styles
+  {
+    variants: {
+      variant: {
+        primary: "bg-customTeal text-white  hover:bg-customTealLight",
+        secondary:
+          "bg-transparent text-customTeal hover:bg-customTealLight hover:text-white border border-customTeal border-2",
+
+        outline:
+          " bg-transparent text-white border border-white hover:bg-gray-100/10",
+
+        ghost: "bg-transparent text-customTeal",
+      },
+      size: {
+        sm: "px-2 py-1 text-md",
+        md: "px-4 py-2 text-lg",
+        lg: "px-6 py-3 text-xl",
+      },
+      loading: {
+        true: "opacity-50 pointer-events-none",
+      },
+      shape: {
+        block: "rounded-none",
+        curved: "rounded-md",
+        rounded: "rounded-[1000px]",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+      shape: "curved",
+    },
+  }
+);
+
+// Button props
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  loading?: boolean;
+}
+
+// Button component
+const Button: React.FC<ButtonProps> = ({
+  className,
+  variant,
+  size,
+  loading,
+  shape,
+  ...props
+}) => {
+  return (
+    <button
+      className={clsx(buttonVariants({ variant, size, shape }), className)}
+      {...props}
+    >
+      {loading ? "loading" : props.children}
+    </button>
+  );
+};
+
+export default Button;
