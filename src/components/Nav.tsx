6c7be3bd0371096554b5f8f4e1bc6a047043f9f5
kaleabSolomon <kaleabslmn@gmail.com>
2024-12-03 21:05:23 +0300

    Enhance navigation component with gradient background and updated text styles; modify button component for bold text

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,8 +3,8 @@ import Button from "./ui/Button";
 
 const Nav = () => {
   return (
-    <div className="fixed top-0 left-0 w-full z-50 sm:px-28 px-8 py-4 bg-transparent text-md flex items-center justify-between text-white">
-      <div className="flex gap-x-8 items-center">
+    <div className="fixed top-0 left-0 w-full z-50 sm:px-28 px-8 py-4 bg-transparent text-md flex items-center justify-between text-white bg-gradient-to-b from-black/60 to-gray-100/0 ">
+      <div className="flex gap-x-8 items-center text-sm">
         <h1 className="text-xl  font-inter text-customTeal ">
           Kena<span className="font-bold">Lib</span>
         </h1>
@@ -13,16 +13,17 @@ const Nav = () => {
         <h2>Charity</h2>
         <h2>Events</h2>
       </div>
-      <div>
+      <div className="flex gap-1 items-center">
         <Button
           variant="ghost"
           size="md"
           shape="rounded"
           className="flex gap-1"
         >
-          Signin
-          <PiSignInBold />{" "}
+          <PiSignInBold /> Signin
         </Button>
+
+        <h1 className="text-sm">(+251)9-4210-2626</h1>
       </div>
     </div>
   );
