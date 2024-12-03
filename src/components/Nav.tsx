ebfff820ca389047de33fd0b691832c5b3faa7ab
kaleabSolomon <kaleabslmn@gmail.com>
2024-12-03 16:30:08 +0300

    create the nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,6 @@
+import { PiSignInBold } from "react-icons/pi";
+import Button from "./ui/Button";
+
 const Nav = () => {
   return (
     <div className="fixed top-0 left-0 w-full z-50 sm:px-28 px-8 py-4 bg-transparent text-md flex items-center justify-between text-white">
@@ -11,7 +14,15 @@ const Nav = () => {
         <h2>Events</h2>
       </div>
       <div>
-        <button>sign in</button>
+        <Button
+          variant="ghost"
+          size="md"
+          shape="rounded"
+          className="flex gap-1"
+        >
+          Signin
+          <PiSignInBold />{" "}
+        </Button>
       </div>
     </div>
   );
