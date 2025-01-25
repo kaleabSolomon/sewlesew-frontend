446d3b57d56bb55cf4f18beb27ad788572ce262d
kaleabSolomon <kaleabslmn@gmail.com>
2025-01-25 16:36:39 +0300

    Add axios instance with request interceptor for token management

---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import Cookie from "js-cookie";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3333/api",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  if (config.url?.includes("refresh")) {
+    const token = Cookie.get("refresh_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } else {
+    const token = Cookie.get("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
+export default axiosInstance;
