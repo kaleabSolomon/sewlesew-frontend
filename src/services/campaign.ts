ff7df11cb753729403bd8451d8c7adc771c5d98a
kaleabSolomon <kaleabslmn@gmail.com>
2025-01-25 16:36:56 +0300

    Add getCampaigns function to fetch campaign data with error handling

---
+++ src/services/campaign.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import axiosInstance from "./axiosInstance";
+
+export const getCampaigns = async (
+  onIsLoading: (isLoading: boolean) => void,
+  onError: (err: string) => void
+) => {
+  onIsLoading(true);
+  onError("");
+
+  try {
+    const res = await axiosInstance.get("/campaign", {
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    });
+
+    return res.data.data;
+  } catch (err) {
+    onIsLoading(false);
+
+    if (axios.isAxiosError(err)) {
+      onError(err.response?.data.message);
+      console.error(err.response?.data);
+    } else {
+      onError("An unexpected error occurred.");
+    }
+    throw err;
+  } finally {
+    onIsLoading(false);
+  }
+};
