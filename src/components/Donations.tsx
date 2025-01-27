ec9c04b7b13b7f2cb144e4adf4fe551451d34d24
kaleabSolomon <kaleabslmn@gmail.com>
2025-01-27 21:19:01 +0300

    Refactor Donations component to utilize CampaignContext; remove props for loading and error handling; implement pagination with state management

--- src/components/Donations.tsx
+++ src/components/Donations.tsx
@@ -4,36 +4,39 @@ import Campaigns from "./Campaigns";
 
 import { GrLinkNext } from "react-icons/gr";
 import Pagination from "./ui/pagination";
-import { campaignMetadata, CampaignPreview } from "@/types/campaign";
-import { FC } from "react";
-interface DonationProps {
-  isLoading: boolean;
-  error: string;
-  campaigns: CampaignPreview[];
-  meta: campaignMetadata;
-}
+import { useCampaignContext } from "@/context/campaignContext";
+import { useEffect, useState } from "react";
 
-const Donations: FC<DonationProps> = ({
-  isLoading,
-  error,
-  campaigns,
-  meta,
-}) => {
+const Donations = () => {
+  const { campaigns, isLoading, error, meta, fetchCampaigns } =
+    useCampaignContext();
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    fetchCampaigns(page);
+  };
+  useEffect(() => {
+    fetchCampaigns(currentPage);
+  }, [currentPage]);
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="font-semibold text-4xl">
         Open <span className="text-customTeal ">donations</span>
       </h1>
       <div className="flex items-center md:flex-col mb-6 ">
-        <SearchBar />
-        <Categories />
+        <SearchBar fetchCampaigns={fetchCampaigns} />
+        <Categories fetchCampaigns={fetchCampaigns} />
       </div>
       <Campaigns isLoading={isLoading} error={error} campaigns={campaigns} />
       <div className=" flex justify-between w-full mt-8 mb-24 ">
         <div>
           <Pagination
-            currentPage={meta.currentPage}
-            totalPages={meta.totalPages}
+            currentPage={currentPage}
+            totalPages={meta?.totalPages || 1} // Get total pages from metadata
+            onPageChange={handlePageChange}
           />
         </div>
 
