399235f49c6f67201a36175b5f776dc5b99b1c6b
dagitinsu <dagmawitinsae834@gmail.com>
2025-01-31 14:54:58 +0300

    change text on signin page

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -39,9 +39,11 @@ const SignIn = () => {
     <div className="flex w-full h-full">
       <div className="hidden xl:flex xl:flex-col w-[40%] h-screen justify-center items-center bg-customTealLight text-white ">
         <h1 className="text-6xl font-bold font-pds mb-10 tracking-widest">
-          JACA
+          SEWLESEW
         </h1>
-        <p className="text-3xl font-semibold">Connect Beyond Words</p>
+        <p className="text-3xl font-semibold">
+          Happiness comes from your action
+        </p>
       </div>
       <div className="w-full xl:w-[60%] h-screen flex flex-col justify-center items-center">
         <div className="bg-white p-6 rounded-2xl shadow-lg w-96">
