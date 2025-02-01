a5f00f70f597cd74663bd25e6574103f7d0859d8
dagitinsu <dagmawitinsae834@gmail.com>
2025-02-01 10:39:43 +0300

    Add jwt-decode library and implement authentication utility functions

---
+++ src/utils/auth.ts
@@ -0,0 +1,39 @@
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
+
+export const isAuthenticated = (): boolean => {
+  const decodedToken = decodeToken("access_token");
+  if (!decodedToken) return false;
+  const isAuthed = decodedToken.exp
+    ? decodedToken.exp > Date.now() / 1000
+    : false;
+
+  return isAuthed;
+};
+
+export function getAuthData() {
+  return {
+    accessToken: Cookies.get("access_token"),
+    refreshToken: Cookies.get("refresh_token"),
+  };
+}
+
+export function decodeToken(tokenName: string) {
+  const token = Cookies.get(tokenName);
+
+  if (!token) return null;
+
+  const decodedToken = jwtDecode(token);
+
+  return decodedToken;
+}
+export function getTokenExpiryTime(token: string) {
+  try {
+    const decoded = jwtDecode(token);
+    const now = Date.now() / 1000;
+    return decoded.exp ? decoded.exp - now : 0;
+  } catch (error) {
+    console.error("Failed to decode token:", error);
+    return 0; // Treat invalid token as expired
+  }
+}
