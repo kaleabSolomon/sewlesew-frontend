bb7ea533afa1c7d1e8b1e139ae63d4d340c9e65c
dagitinsu <dagmawitinsae834@gmail.com>
2025-02-01 10:39:50 +0300

    Implement sign-in service with email and phone number validation

---
+++ src/services/auth.ts
@@ -0,0 +1,40 @@
+import Cookie from "js-cookie";
+import axiosInstance from "./axiosInstance";
+import axios from "axios";
+import { SignInResponse } from "@/types/auth";
+import { isValidEmail } from "@/utils/validators";
+
+export const signIn = async (identifier: string, password: string) => {
+  try {
+    const identifierBody = isValidEmail(identifier)
+      ? { email: identifier }
+      : { phoenNumber: identifier };
+    const reqBody = {
+      ...identifierBody,
+      password,
+    };
+
+    const { data } = await axiosInstance.post<SignInResponse>(
+      "/auth/local/signin",
+      reqBody
+    );
+
+    // Store tokens in cookies
+    Cookie.set("access_token", data.access_token, {
+      secure: true,
+      sameSite: "Strict",
+    });
+    Cookie.set("refresh_token", data.refresh_token, {
+      secure: true,
+      sameSite: "Strict",
+    });
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Sign-in error:", error.response?.data || error.message);
+      throw new Error(error.response?.data?.message || "Sign-in failed");
+    } else {
+      console.error("Sign-in error:", error);
+      throw new Error("Sign-in failed");
+    }
+  }
+};
