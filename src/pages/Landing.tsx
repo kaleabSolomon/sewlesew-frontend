bf8097bfb0e7ffca673fb57463d48859136108fc
dagitinsu <dagmawitinsae834@gmail.com>
2025-02-04 16:16:19 +0300

    Remove Nav and Footer components from Landing page for streamlined layout

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,15 +1,12 @@
 import Hero from "@/components/Hero";
-import Nav from "@/components/Nav";
 import Donations from "@/components/Donations";
 import Faq from "@/components/Faq";
 import Stats from "@/components/Stats";
 import Partners from "@/components/Partners";
-import Footer from "@/components/Footer";
 
 const Landing = () => {
   return (
     <div>
-      <Nav />
       <div className="w-screen h-[1000px] bg-heroImg bg-center bg-cover bg-no-repeat ">
         <div className="h-full w-full bg-black bg-clip-padding backdrop-filter bg-opacity-70">
           <Hero />
@@ -24,7 +21,6 @@ const Landing = () => {
         <Stats />
         <Partners />
       </div>
-      <Footer />
     </div>
   );
 };
