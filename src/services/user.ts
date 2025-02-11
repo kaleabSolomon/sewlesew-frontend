e64c41a1428371f1d38fdf1312f9b654d317906b
kaleabSolomon <kaleabslmn@gmail.com>
2025-02-11 16:28:34 +0300

    Add updateUser function to handle user data updates with error handling and loading state management

--- src/services/user.ts
+++ src/services/user.ts
@@ -22,3 +22,31 @@ export const getUserBrief = async () => {
     throw new Error("An unexpected error occurred");
   }
 };
+
+export const updateUser = async (
+  formData: FormData,
+  onIsLoading: (isLoading: boolean) => void,
+  onError: (err: string) => void
+) => {
+  onIsLoading(true);
+  try {
+    const { data } = await axiosInstance.patch("/user", formData, {
+      headers: {
+        Authorization: `Bearer ${Cookie.get("access_token")}`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log(data);
+    return data;
+  } catch (err) {
+    onIsLoading(false);
+    if (axios.isAxiosError(err)) {
+      console.error(err.response?.data);
+      onError(err.response?.data.message || "Could not Create Campaign."); // Log for debugging
+      throw new Error(err.response?.data.message || "An error occurred");
+    }
+    throw new Error("An unexpected error occurred");
+  } finally {
+    onIsLoading(false);
+  }
+};
