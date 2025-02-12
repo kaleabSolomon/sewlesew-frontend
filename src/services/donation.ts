2c56db96305b0171e8ad9d0e250b2376fd4de8bf
kaleabSolomon <kaleabslmn@gmail.com>
2025-02-13 02:10:49 +0300

    Add guest donation functionality and enhance donation service with error handling

--- src/services/donation.ts
+++ src/services/donation.ts
@@ -1,13 +1,50 @@
 import { IDonation } from "@/types/donation";
 import axios from "axios";
 import axiosInstance from "./axiosInstance";
+import Cookie from "js-cookie";
 
+export const donateGuest = async (donation: IDonation, campaignId: string) => {
+  try {
+    const { data } = await axiosInstance.post(
+      `/donation/guest/${campaignId}`,
+      donation
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const errData = error.response.data;
+      console.error("fetching error: ", errData);
+    } else {
+      console.error("Unexpected error:", error);
+    }
+  }
+};
 export const donate = async (donation: IDonation, campaignId: string) => {
   try {
     const { data } = await axiosInstance.post(
-      `/donate/${campaignId}`,
-      donation
+      `/donation/${campaignId}`,
+      donation,
+      {
+        headers: {
+          Authorization: `Bearer ${Cookie.get("access_token")}`,
+          "Content-Type": "Application/json",
+        },
+      }
     );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const errData = error.response.data;
+      console.error("fetching error: ", errData);
+    } else {
+      console.error("Unexpected error:", error);
+    }
+  }
+};
+
+export const getDonationsByCampaign = async (campaignId: string) => {
+  try {
+    const { data } = await axiosInstance.get(`/donation/${campaignId}`);
     return data;
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
